529c0bdaf090e966190e02bf14784d332119abdc
dddushesss <37773701+dddushesss@users.noreply.github.com>
2021-11-19 21:03:06 +0300

    BuildExtention

---
+++ Assets/Extension/BuilderExtension.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+namespace MVCExample
+{
+    public static partial class BuilderExtension
+    {
+
+        public static GameObject AddNavMeshSurface(this GameObject gameObject)
+        {
+            var component = gameObject.GetOrAddComponent<NavMeshSurface>();
+            return gameObject;
+        } 
+        public static GameObject AddNavMeshAgent(this GameObject gameObject)
+        {
+            var component = gameObject.GetOrAddComponent<NavMeshAgent>();
+            return gameObject;
+        }
+        public static GameObject SetName(this GameObject gameObject, string name)
+        {
+            gameObject.name = name;
+            return gameObject;
+        }
+
+        private static T GetOrAddComponent<T>(this GameObject gameObject) where T : Component
+        {
+            var result = gameObject.GetComponent<T>();
+            if (!result)
+            {
+                result = gameObject.AddComponent<T>();
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Assets/MenuView.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuView : MonoBehaviour
+{
+    public Button StartButton;
+    public Button ExitButton;
+    public Button MenuButton;
+    public GameObject pauseMenuBody;
+}
