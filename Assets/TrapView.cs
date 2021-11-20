ca2195f2953b97d0735ea2327d7bfdccbb4c3184
dddushesss <37773701+dddushesss@users.noreply.github.com>
2021-11-20 14:45:27 +0300

    colors

---
+++ Assets/TrapView.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrapView : MonoBehaviour
+{
+    private void OnTriggerStay(Collider other)
+    {
+        throw new NotImplementedException();
+    }
+}
